63fbcf8ad6d6c599743b3b09493e10373cdcdb8d
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-29 16:06:48 -0300

    routes

---
+++ src/app/pages/purchase/purchase.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase',
+  template: `
+    <p>
+      purchase works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PurchaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
