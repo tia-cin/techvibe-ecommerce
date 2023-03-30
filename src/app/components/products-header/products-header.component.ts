d02ee1dc01cf9e3127646ac4d16891130c4ba189
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-30 15:01:55 -0300

    products header functionality

--- src/app/components/products-header/products-header.component.ts
+++ src/app/components/products-header/products-header.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
 @Component({
   selector: "app-products-header",
   templateUrl: "./products-header.component.html",
 })
 export class ProductsHeaderComponent implements OnInit {
+  @Output() colsChange = new EventEmitter<number>();
+
   sort = "A - Z";
+  itemsCount = 6;
+
   constructor() {}
 
   ngOnInit(): void {}
 
   onSortUpdated(sortBy: string): void {
     this.sort = sortBy;
+  }
+
+  onItemsDisplayed(items: number): void {
+    this.itemsCount = items;
+  }
+
+  onColsDisplay(cols: number): void {
+    this.colsChange.emit(cols);
   }
 }
