01df8b06cd1cc553916e0a420a7a9466fb63e011
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-30 18:03:32 -0300

    cart setup

--- src/app/pages/purchase/purchase.component.ts
+++ src/app/pages/purchase/purchase.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
 @Component({
-  selector: 'app-purchase',
-  template: `
-    <p>
-      purchase works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: "app-purchase",
+  templateUrl: "./purchase.component.html",
 })
 export class PurchaseComponent implements OnInit {
+  cart = { items: [] };
+  dataSource = [];
+  cartSubscription: undefined;
+  currCols: string[] = [
+    "product",
+    "name",
+    "price",
+    "quantity",
+    "total",
+    "action",
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
