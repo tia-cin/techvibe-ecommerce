4e42e098c992c7eb0c4d041fe7f53d725d72cc6c
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-30 16:51:09 -0300

    setup product card grid

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
+const ROW_HEIGHT: { [id: number]: number } = { 1: 400, 3: 355, 5: 380 };
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
 })
 export class HomeComponent implements OnInit {
-  cols = 3;
+  cols: number = 3;
+  rowHeight: number = ROW_HEIGHT[this.cols];
   category: string | undefined;
 
   constructor() {}
