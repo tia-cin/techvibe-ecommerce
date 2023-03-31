906cad8effbb80feabee1262244a29ec0f1afb65
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-31 11:35:04 -0300

    purchase comp setup

--- src/app/pages/purchase/purchase.component.ts
+++ src/app/pages/purchase/purchase.component.ts
@@ -1,14 +1,17 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit, OnDestroy } from "@angular/core";
+import { PurchaseService } from "src/app/services/purchase.service";
+import { Cart, CartItem } from "src/app/types";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-purchase",
   templateUrl: "./purchase.component.html",
 })
 export class PurchaseComponent implements OnInit {
-  cart = { items: [] };
-  dataSource = [];
-  cartSubscription: undefined;
+  cart: Cart = { items: [] };
+  dataSource: CartItem[] = [];
+  cartSubscription: undefined | Subscription;
   currCols: string[] = [
     "product",
     "name",
@@ -17,7 +20,17 @@ export class PurchaseComponent implements OnInit {
     "total",
     "action",
   ];
-  constructor() {}
+  constructor(
+    private purchaseService: PurchaseService,
+    private http: HttpClient
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.cartSubscription = this.purchaseService.cart.subscribe(
+      (_cart: Cart) => {
+        this.cart = _cart;
+        this.dataSource = _cart.items;
+      }
+    );
+  }
 }
