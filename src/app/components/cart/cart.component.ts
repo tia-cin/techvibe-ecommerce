2879ee55303b29f9cd76dd17d483266414cb99a0
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-04 12:11:40 -0300

    purchase page - home page - cart logic

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { PurchaseService } from "src/app/services/purchase.service";
+import { Cart, CartItem } from "src/app/types";
 
 @Component({
   selector: "app-cart",
   templateUrl: "./cart.component.html",
 })
 export class CartComponent implements OnInit {
-  constructor() {}
+  @Input() itemsQuantity: number = 0;
+  @Input() cart: Cart = { items: [] };
+
+  constructor(private cartService: PurchaseService) {}
 
   ngOnInit(): void {}
+
+  getTotal(items: CartItem[]): number {
+    return this.cartService.getTotal(items);
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from "@angular/core";
-import { CartComponent } from "../cart/cart.component";
+import { Component, Input, OnInit } from "@angular/core";
+import { Cart } from "src/app/types";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
 })
-export class HeaderComponent implements OnInit {
-  constructor() {}
+export class HeaderComponent {
+  @Input()
+  get cart(): Cart {
+    return this._cart;
+  }
 
-  ngOnInit(): void {}
+  set cart(cart: Cart) {
+    this._cart = cart;
+
+    this.itemsQuantity = cart.items
+      .map((item) => item.quantity)
+      .reduce((prev, curr) => prev + curr, 0);
+  }
+
+  private _cart: Cart = { items: [] };
+  itemsQuantity = 0;
+
+  constructor() {}
 }
