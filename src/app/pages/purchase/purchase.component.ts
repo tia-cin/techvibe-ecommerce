5042c124267923c6507c697205f3dd6db37dbf02
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-04 10:53:25 -0300

    purchase page structure

--- src/app/pages/purchase/purchase.component.ts
+++ src/app/pages/purchase/purchase.component.ts
@@ -20,30 +20,52 @@ export class PurchaseComponent implements OnInit {
     "total",
     "action",
   ];
-  constructor() {}
+  constructor(
+    private purchaseService: PurchaseService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
-    // this.cartSubscription = this.purchaseService.cart.subscribe(
-    //   (_cart: Cart) => {
-    //     this.cart = _cart;
-    //     this.dataSource = _cart.items;
-    //   }
-    // );
+    this.cartSubscription = this.purchaseService.cart.subscribe(
+      (_cart: Cart) => {
+        this.cart = _cart;
+        this.dataSource = _cart.items;
+      }
+    );
   }
 
-  // getTotal(items: CartItem[]): number {
-  //   return this.purchaseService.getTotal(items);
+  getTotal(items: CartItem[]): number {
+    return this.purchaseService.getTotal(items);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.purchaseService.addToCart(item);
+  }
+
+  onRemoveItem(item: CartItem): void {
+    this.purchaseService.removeQuantity(item);
+  }
+
+  onEmptyCart(): void {
+    this.purchaseService.emptyCart();
+  }
+
+  // onCheckout(): void {
+  //   this.http
+  //     .post('http://localhost:4242/checkout', {
+  //       items: this.cart.items,
+  //     })
+  //     .subscribe(async (res: any) => {
+  //       let stripe = await loadStripe('your token');
+  //       stripe?.redirectToCheckout({
+  //         sessionId: res.id,
+  //       });
+  //     });
   // }
 
-  // onAddQuantity(item: CartItem): void {
-  //   this.purchaseService.addToCart(item);
-  // }
-
-  // onRemoveItem(item: CartItem): void {
-  //   this.purchaseService.removeQuantity(item);
-  // }
-
-  // onEmptyCart(): void {
-  //   this.purchaseService.emptyCart();
+  // ngOnDestroy() {
+  //   if (this.cartSubscription) {
+  //     this.cartSubscription.unsubscribe();
+  //   }
   // }
 }
