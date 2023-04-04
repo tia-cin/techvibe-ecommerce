78656d20ac59e8ebb0f60b08a8a254f4eeb8fc90
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-04 11:44:09 -0300

    purchase page - card logic

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { Product } from "src/app/types";
 
 @Component({
   selector: "app-card",
@@ -6,8 +7,23 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class CardComponent implements OnInit {
   @Input() fullWidth = false;
+  @Output() addToCart = new EventEmitter();
+
+  product: Product | undefined = {
+    id: 1,
+    name: "product name",
+    price: 100,
+    category: "smartphone",
+    details: "Et voluptate in duis nostrud quis officia anim nisi sit.",
+    image: "https://via.placeholder.com/150",
+    slug: "product-name",
+  };
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
+  }
 }
