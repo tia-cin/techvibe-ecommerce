e9c89d876880109f4653cabbc215f2250700da3f
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-10 15:12:55 -0300

    Input banner to footer

--- src/app/components/footer-banner/footer-banner.component.ts
+++ src/app/components/footer-banner/footer-banner.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { Banner } from "src/app/types";
 
 @Component({
   selector: "app-footer-banner",
   templateUrl: "./footer-banner.component.html",
 })
 export class FooterBannerComponent implements OnInit {
+  @Input() banner: Banner | undefined;
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/components/header-banner/header-banner.component.ts
+++ src/app/components/header-banner/header-banner.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { Banner } from "src/app/types";
 
 @Component({
   selector: "app-header-banner",
   templateUrl: "./header-banner.component.html",
 })
 export class HeaderBannerComponent implements OnInit {
+  @Input() banner: Banner | undefined;
+
   constructor() {}
 
   ngOnInit(): void {}
