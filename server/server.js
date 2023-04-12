ed22bb83cf853206cd830528b1750ee837d99239
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-12 10:58:37 -0300

    Server stripe logic

--- server/server.js
+++ server/server.js
@@ -4,12 +4,10 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
-
 app.use(express.static("public"));
-
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ origin: true, credentials: true }));
 
 const stripe = require("stripe")(
   "sk_test_51LqFYzENi5vsUdl188ugvsytoeQVvq0XPQKyLEZFQMzb1Dkw7uKiR3QzkEv3eoKYiu8L7KiVtBD86HFyTuqumADF00Ru0Gfppi"
@@ -18,13 +16,61 @@ const stripe = require("stripe")(
 app.post("/checkout", async (req, res, next) => {
   try {
     const session = await stripe.checkout.session.create({
-      line_items: req.body.items.map((item) => ({
-        currency: "usd",
-        product_data: {
-          name: item.name,
-          images: [item.image],
+      payment_method_types: ["card"],
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA"],
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 0,
+              currency: "usd",
+            },
+            display_name: "Free shipping",
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 5,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 7,
+              },
+            },
+          },
         },
-        unit_amount: item.price * 100,
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 1500,
+              currency: "usd",
+            },
+            display_name: "Next day air",
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 1,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 1,
+              },
+            },
+          },
+        },
+      ],
+      line_items: req.body.items.map((item) => ({
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.name,
+            images: [item.image],
+          },
+          unit_amount: item.price * 100,
+        },
       })),
       mode: "payment",
       success_url: "http://localhost:4201/success.html",
