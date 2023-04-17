63c81b2d8288f597125350713c51e9e55d142b13
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-17 14:39:08 -0300

    Banners - header func

--- src/app/components/header-banner/header-banner.component.ts
+++ src/app/components/header-banner/header-banner.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, OnInit, Input } from "@angular/core";
 import { Banner } from "src/app/types";
+import { loadStripe } from "@stripe/stripe-js";
 
 @Component({
   selector: "app-header-banner",
@@ -8,7 +10,30 @@ import { Banner } from "src/app/types";
 export class HeaderBannerComponent implements OnInit {
   @Input() banner: Banner | undefined;
 
-  constructor() {}
+  constructor(private client: HttpClient) {}
 
   ngOnInit(): void {}
+
+  onCheckout(): void {
+    this.client
+      .post("http://localhost:4201/checkout", {
+        items: [
+          {
+            image: this.banner?.image,
+            name: this.banner?.product,
+            price: 350,
+            quantity: 1,
+            id: 1,
+          },
+        ],
+      })
+      .subscribe(async (res: any) => {
+        let stripe = await loadStripe(
+          "pk_test_51LqFYzENi5vsUdl1S6DlnTj64Jppk67ktJiaASMWUwXgYyEGgZxtHtcdvDINlnNK537BxkTeoacuedDo8Hjpe8X600jUwt0g3d"
+        );
+        stripe?.redirectToCheckout({
+          sessionId: res.id,
+        });
+      });
+  }
 }
